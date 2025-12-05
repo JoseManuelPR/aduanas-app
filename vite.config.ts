4aff8431dfe5091e5f09df9ea3a6e4175dc72ff5
José Manuel Puicón Rodas <josemanuelpr23@gmail.com>
2025-12-05 10:06:21 -0500

    Aadd Vercel deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    cssCodeSplit: false, // Asegura que todos los estilos se incluyan en un solo archivo
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Evita dividir el código en chunks que puedan causar problemas
+      },
+    },
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
